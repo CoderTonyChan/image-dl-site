5392be6a0826836592713670666e8f3760e4b965
coderTonyChan <101992315@qq.com>
2018-11-01 10:22:59 +0800

    Api规划

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# image-dl-site
-node cli for downloading images in some site.
+## image-dl-site node cli
+
+### install
+
+```shell
+npm install --global image-dl-site
+```
+
+### Options
+
+``image-dl url [...url]``，后面可带多个 url，url 需要用引号包括起来。
+
+- ``-c, --config [path]``：配置文件的路径
+- ``-o, --output [path]``：输出路径，默认保存到当前执行目录
+- ``-m, --meizitu [id]``：下载妹子图post的所有图片
+- ``-n, --nheitai [id]``：下载nheitai该页的所有图片
---
+++ index.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+const mkdirp = require('mkdirp');
+const cwd = process.cwd();
+
+function download (options, showLog, cb) {
+    options = preprocessOptions(options, showLog);
+    if (options === null) return;
+
+    let allDone = (function () {
+        let count = options.urls.length;
+        let callback = cb || function(){};
+        return function () {
+            showLog && console.log('Finished. ' + count + ' images are downloaded.');
+            callback();
+        }
+    })();
+
+    // 增加option
+
+    showLog && console.log('downloading...')
+    let isSingle = options.urls.length === 1;
+    let count = options.urls.length;
+    options.urls.forEach(url => {
+        let filepath;
+        let filename = url.split('/').pop();
+        if (isSingle && options.filename) {
+            filename = options.filename;
+        }
+        filepath = path.resolve(options.output, filename);
+        if (fs.existsSync(filepath)) {
+            filename = filename.split('.');
+            filename.splice(-1, 0, randomString());
+            filename = filename.join('.');
+            filepath = path.resolve(options.output, filename);
+        }
+        request(url)
+            .pipe(fs.createWriteStream(filepath))
+            .on('close', () => {
+                showLog && console.log(url + ': downloaded')
+                count--;
+                count === 0 && allDone();
+            });
+    });
+}
+
+function preprocessOptions (options, showLog) {
+    let opts = Object.assign({}, options);
+
+    if (!opts.urls || !opts.urls.length) return null;
+
+    opts.output = path.resolve(cwd, opts.output);
+    opts.filename = null;
+    if (opts.urls.length === 1 && path.extname(opts.output) !== '') {
+        opts.filename = path.basename(opts.output);
+        opts.output = path.dirname(opts.output);
+    }
+
+    if (!fs.existsSync(opts.output)) {
+        try {
+            mkdirp(opts.output);
+        } catch (err) {
+            showLog && console.error(err);
+            return null;
+        }
+    }
+    return opts;
+}
+
+function randomString () {
+    return Math.random().toString(36).slice(2, 8);
+}
+
+module.exports = download;
